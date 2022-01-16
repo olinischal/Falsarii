adc82da4d8c3e4ae5693c3ae449f16d288b3d162
Nischal <olinischal@gmail.com>
2022-01-15 19:16:17 -0600

    initial commit

---
+++ Backend/README.md
@@ -0,0 +1,15 @@
+## SPRING BOOT Starter Project
+Initial project setup for Spring Boot Web-Application using MVC,JPA,Hibernate
+
+##Recommended Development Environment
+- Intellij
+
+##Description
+Falsarii connects to Postgres database and implements GetMapping to retrieve data.
+
+##Connect
+localhost:8080
+
+#connect to local database
+http://localhost:8080/api/students
+
---
+++ Backend/src/main/java/com/example/student/controller/StudentController.java
@@ -0,0 +1,55 @@
+package com.example.student.controller;
+
+import java.util.List;
+
+import com.example.student.entity.Student;
+import com.example.student.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository eRepo;
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return eRepo.findAll();
+    }
+
+    @GetMapping("/Students/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return eRepo.findById(id).get();
+    }
+
+    @PostMapping("/Students")
+    public Student saveStudentDetails(@RequestBody Student Student) {
+        return eRepo.save(Student);
+    }
+
+    @PutMapping("/Students")
+    public Student updateStudent(@RequestBody Student Student) {
+        return eRepo.save(Student);
+    }
+
+    @DeleteMapping("/Students/{id}")
+    public ResponseEntity<HttpStatus> deleteStudentById(@PathVariable Long id) {
+        eRepo.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
