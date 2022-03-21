ea6d4aa684f4e8c7b115abbf7f5f9455222ffa35
Ramesh Chapagain <chapagainramesh1043@gmail.com>
2022-03-21 16:55:28 -0500

    commit

--- Frontend/src/components/signup/NewSignUp.tsx
+++ Frontend/src/components/signup/NewSignUp.tsx
@@ -6,14 +6,14 @@ function NewSignUp() {
       <div className="row">
         <div className="col-md-4 my-auto logo-edit">
           {/* <img className="img-fluid logo" src={require("./NAFA_Logo.PNG")} alt=""/> */}
-          <p style={{color:'black', fontSize:'18px'}}>
+          {/* <p style={{color:'black', fontSize:'18px'}}> */}
             <ul style={{listStyleType:'none'}}>
               <li style={{fontSize:'25px'}}><b>Register Your New NAFA Account</b></li>
               <li>Fill out the User Registration form to create your account.</li>
               <li>Enter valid phone and email address.</li>
               <li>You should see remainder of account creation on successfully registering.</li>
             </ul>
-          </p>
+          {/* </p> */}
         </div>
         <div className="col-md-4 formSignUp">
           <Signup />
