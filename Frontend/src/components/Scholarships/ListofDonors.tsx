ca17f9a8e9c86915f993a4b45803fe324c67da4e
olinischal <46114270+olinischal@users.noreply.github.com>
2022-05-02 22:20:33 -0500

    donor list

--- Frontend/src/components/Scholarships/ListofDonors.tsx
+++ Frontend/src/components/Scholarships/ListofDonors.tsx
@@ -5,14 +5,14 @@ import { getScholarshipDonateList } from "../../services/api";
 
 const ListOfDonors = ({user}) => {
   
-    //this will update all the donor list in the scholarship page
-    // useEffect(()=> {
-    //     getScholarshipDonateList(user.userId).then((res) => {
-    //         console.log('The donor list has been successfull', res);
-    //     }) .catch((error) => {
-    //         console.log("Could not retrieve donor list.", error);
-    //       });
-    // },[])
+   // this will update all the donor list in the scholarship page
+    useEffect(()=> {
+        getScholarshipDonateList(user.userId).then((res) => {
+            console.log('The donor list has been successfull', res);
+        }) .catch((error) => {
+            console.log("Could not retrieve donor list.", error);
+          });
+    },[])
 
     return (
 
